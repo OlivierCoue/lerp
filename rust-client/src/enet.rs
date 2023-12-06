39587833dfb2d0b917c380125651bbbb2566a32f
Olivier Coué <olivier28.coue@gmail.com>
2023-12-06 23:41:13 +0100

    fix: exported project

--- rust-client/src/enet.rs
+++ rust-client/src/enet.rs
@@ -17,7 +17,7 @@ pub struct ENetPeerPtrWrapper(*mut _ENetPeer);
 unsafe impl Sync for ENetPeerPtrWrapper {}
 unsafe impl Send for ENetPeerPtrWrapper {}
 
-const ADDRESS: &str = "127.0.0.1";
+const ADDRESS: &str = "35.181.43.91";
 const PORT: u16 = 34254;
 
 pub fn enet_start(
--- rust-client/src/lib.rs
+++ rust-client/src/lib.rs
@@ -1,11 +1,12 @@
 use godot::prelude::*;
 
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
 pub mod enet;
 pub mod entity;
 pub mod network;
 pub mod play_node;
 pub mod root;
-struct MyExtension;
-
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {}
