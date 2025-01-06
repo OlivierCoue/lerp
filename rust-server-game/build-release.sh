4dd42a7b2aa1f343310acdc8cea97b04a5621daa
Olivier Coue <olivier28.coue@gmail.com>
2025-01-06 22:14:21 +0000

    wip

--- rust-server-game/build-release.sh
+++ rust-server-game/build-release.sh
@@ -6,5 +6,5 @@ cp ./rust-server-game/Dockerfile ./rust-server-game/dist/release
 cp ./target/release/rust-server-game ./rust-server-game/dist/release
 
 # docker rmi -f rust-server-game:1
-# docker build ./rust-server-game/dist/debug -t rust-server-game:1
+# docker build ./rust-server-game/dist/release -t rust-server-game:1
 # docker run --rm -p 34255:34255/udp rust-server-game:1
