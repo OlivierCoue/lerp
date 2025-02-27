bbb1fb2a37fd7d6c32dceea65bc24ededdcf4978
GuiguiBlitz <guiguiblitz@gmail.com>
2025-02-27 19:43:31 +0000

    feat: create ButtonAction component

--- lerp-map-editor/src/main.rs
+++ lerp-map-editor/src/main.rs
@@ -10,23 +10,28 @@ use lerp_map_gen::*;
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+const MAP_SIZE: UVec2 = UVec2::new(100, 100);
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, MapGenPlugin))
         .add_systems(Startup, (setup, render_map).chain())
-        .add_systems(Update, (zoom_camera, drag_camera, button_system))
+        .add_systems(
+            Update,
+            (zoom_camera, drag_camera, button_system, render_map),
+        )
         // .add_systems(Update, sprite_movement)
         .run();
 }
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
+#[derive(Component, Clone, Copy)]
+enum ButtonAction {
+    ResetMap,
+    GenerateBSP,
+    // Add more actions as needed
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut<Map>) {
+fn setup(mut commands: Commands, mut map: ResMut<Map>) {
     commands.spawn(Camera2d);
     commands
         .spawn(Node {
@@ -34,6 +39,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut
             height: Val::Percent(100.0),
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
             ..default()
         })
         .with_children(|parent| {
@@ -44,7 +50,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut
                         width: Val::Px(150.0),
                         height: Val::Px(65.0),
                         border: UiRect::all(Val::Px(5.0)),
-                        flex_direction: FlexDirection::Row,
+                        flex_direction: FlexDirection::Column,
                         justify_content: JustifyContent::Center,
                         align_items: AlignItems::Center,
 
@@ -53,9 +59,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut
                     BorderColor(Color::BLACK),
                     BorderRadius::MAX,
                     BackgroundColor(NORMAL_BUTTON),
+                    ButtonAction::ResetMap,
                 ))
                 .with_child((
-                    Text::new("Spawn "),
+                    Text::new("Reset"),
+                    TextFont {
+                        // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 33.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+        })
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    Button,
+                    Node {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BorderRadius::MAX,
+                    BackgroundColor(NORMAL_BUTTON),
+                    ButtonAction::GenerateBSP,
+                ))
+                .with_child((
+                    Text::new("GenerateBSP "),
                     TextFont {
                         // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                         font_size: 33.0,
@@ -64,10 +100,18 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut
                     TextColor(Color::srgb(0.9, 0.9, 0.9)),
                 ));
         });
-    map.generate_map(UVec2::new(50, 50));
+    map.generate_map(MAP_SIZE);
 }
 
-fn render_map(mut commands: Commands, map: Res<Map>) {
+fn render_map(
+    mut commands: Commands,
+    map: Res<Map>,
+    mut sprite_query: Query<Entity, With<Sprite>>,
+) {
+    // Clear existing map sprites
+    for entity in sprite_query.iter_mut() {
+        commands.entity(entity).despawn();
+    }
     for x in 0..map.render_grid_size.x {
         for y in 0..map.render_grid_size.y {
             let color = match map.get_tile(&TilePos(UVec2::new(x, y))).unwrap() {
@@ -108,7 +152,6 @@ fn drag_camera(
     mouse_buttons: Res<ButtonInput<MouseButton>>,
     mut last_mouse_position: Local<Option<Vec2>>,
     primary_window_query: Query<&Window, With<PrimaryWindow>>,
-    time: Res<Time>,
 ) {
     let window = primary_window_query.single();
     if mouse_buttons.pressed(MouseButton::Left) {
@@ -137,31 +180,31 @@ fn button_system(
             &Interaction,
             &mut BackgroundColor,
             &mut BorderColor,
-            &Children,
+            &ButtonAction,
         ),
         (Changed<Interaction>, With<Button>),
     >,
-    mut text_query: Query<&mut Text>,
     mut map: ResMut<Map>,
-    mut commands: Commands,
 ) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
+    for (interaction, mut color, mut border_color, button_action) in &mut interaction_query {
         match *interaction {
             Interaction::Pressed => {
-                map.generate_bsp_floor(10, UVec2::new(5, 5));
-                render_map(commands, map.as_ref());
-                **text = "Press".to_string();
                 *color = PRESSED_BUTTON.into();
                 border_color.0 = Color::WHITE;
+                match *button_action {
+                    ButtonAction::GenerateBSP => {
+                        map.generate_bsp_floor(10, UVec2::new(5, 5));
+                    }
+                    ButtonAction::ResetMap => {
+                        map.generate_map(MAP_SIZE);
+                    }
+                }
             }
             Interaction::Hovered => {
-                **text = "Hover".to_string();
                 *color = HOVERED_BUTTON.into();
                 border_color.0 = Color::WHITE;
             }
             Interaction::None => {
-                **text = "Button".to_string();
                 *color = NORMAL_BUTTON.into();
                 border_color.0 = Color::BLACK;
             }
