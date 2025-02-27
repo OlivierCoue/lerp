40f9bdf1e4eb93763723371d0d6519b5e5c5394f
GuiguiBlitz <guiguiblitz@gmail.com>
2025-02-27 19:15:00 +0000

    feat: try button

--- lerp-map-editor/src/main.rs
+++ lerp-map-editor/src/main.rs
@@ -2,16 +2,20 @@ use bevy::{
     input::mouse::MouseButton,
     input::mouse::MouseWheel,
     prelude::*,
+    ui::Interaction,
     window::{PrimaryWindow, Window},
 };
 use lerp_map_gen::*;
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, MapGenPlugin))
         .add_systems(Startup, (setup, render_map).chain())
-        .add_systems(Update, (zoom_camera, drag_camera))
-        // .add_systems(Startup, setup)
+        .add_systems(Update, (zoom_camera, drag_camera, button_system))
         // .add_systems(Update, sprite_movement)
         .run();
 }
@@ -22,37 +26,48 @@ enum Direction {
     Down,
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    mut map: ResMut<Map>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut map: ResMut<Map>) {
     commands.spawn(Camera2d);
-    generate_map(&mut map, UVec2::new(100, 200));
-    // let layout = TextureAtlasLayout::from_grid(UVec2::splat(100), 5, 1, None, None);
-    // let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    // commands.spawn((
-    //     Sprite::from_atlas_image(
-    //         asset_server.load("sprite_du_cul.png"),
-    //         TextureAtlas {
-    //             layout: texture_atlas_layout,
-    //             index: 4,
-    //         },
-    //     ),
-    //     // Sprite::from_image(asset_server.load("branding/icon.png")),
-    //     // Sprite::from_color(),
-    //     Transform::from_xyz(100., 0., 0.),
-    //     Direction::Up,
-    // ));
+    commands
+        .spawn(Node {
+            width: Val::Percent(15.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    Button,
+                    Node {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BorderRadius::MAX,
+                    BackgroundColor(NORMAL_BUTTON),
+                ))
+                .with_child((
+                    Text::new("Spawn "),
+                    TextFont {
+                        // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 33.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+        });
+    map.generate_map(UVec2::new(50, 50));
 }
 
-fn render_map(
-    mut commands: Commands,
-    // asset_server: Res<AssetServer>,
-    // mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    map: Res<Map>,
-) {
+fn render_map(mut commands: Commands, map: Res<Map>) {
     for x in 0..map.render_grid_size.x {
         for y in 0..map.render_grid_size.y {
             let color = match map.get_tile(&TilePos(UVec2::new(x, y))).unwrap() {
@@ -113,5 +128,43 @@ fn drag_camera(
         }
     } else {
         *last_mouse_position = None; // Reset when not dragging
+    }
+}
+
+fn button_system(
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &Children,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut map: ResMut<Map>,
+    mut commands: Commands,
+) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                map.generate_bsp_floor(10, UVec2::new(5, 5));
+                render_map(commands, map.as_ref());
+                **text = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::Hovered => {
+                **text = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                **text = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
     }
 }
--- lerp-map-gen/src/lib.rs
+++ lerp-map-gen/src/lib.rs
@@ -1,4 +1,11 @@
 use bevy::{prelude::*, utils::HashMap};
+use rand::{rng, Rng};
+/// Number of pixels per one meter
+pub const PIXEL_METER: f32 = 32.;
+
+pub const NAV_TILE_SIZE: f32 = PIXEL_METER / 2.;
+pub const RENDER_TO_NAV_TILE_MULTI: u32 = 5;
+pub const RENDER_TILE_SIZE: f32 = NAV_TILE_SIZE * RENDER_TO_NAV_TILE_MULTI as f32;
 
 pub struct MapGenPlugin;
 
@@ -11,7 +18,7 @@ impl Plugin for MapGenPlugin {
 #[derive(Component, PartialEq, Eq, Clone, Copy, Debug, Hash)]
 pub struct TilePos(pub UVec2);
 
-#[derive(Resource, Default)]
+#[derive(Resource, Default, Clone)]
 pub struct Map {
     render_grid: HashMap<TilePos, TileKind>,
     pub render_grid_size: UVec2,
@@ -29,6 +36,89 @@ impl Map {
     pub fn get_tile(&self, pos: &TilePos) -> Option<&TileKind> {
         self.render_grid.get(pos)
     }
+    pub fn generate_bsp_floor(&mut self, iterations: u32, min_size: UVec2) {
+        let mut rng = rng();
+
+        let mut regions = vec![(UVec2::ZERO, self.render_grid_size)];
+
+        for _ in 0..iterations {
+            let mut new_regions = Vec::new();
+            for (start, size) in regions {
+                if size.x < min_size.x || size.y < min_size.y {
+                    new_regions.push((start, size));
+                    continue;
+                }
+
+                let axis = rng.random_range(0..2);
+                let first_region;
+                let second_region;
+
+                if axis == 0 {
+                    let max_threshold = size.x - min_size.x;
+                    if max_threshold <= min_size.x {
+                        new_regions.push((start, size));
+                        continue;
+                    }
+
+                    let threshold = rng.random_range(min_size.x..=max_threshold);
+                    first_region = (start, UVec2::new(threshold, size.y));
+                    second_region = (
+                        UVec2::new(start.x + threshold, start.y),
+                        UVec2::new(size.x - threshold, size.y),
+                    );
+                } else {
+                    let max_threshold = size.y - min_size.y;
+                    if max_threshold <= min_size.y {
+                        new_regions.push((start, size));
+                        continue;
+                    }
+                    let threshold = rng.random_range(min_size.y..=max_threshold);
+                    first_region = (start, UVec2::new(size.x, threshold));
+                    second_region = (
+                        UVec2::new(start.x, start.y + threshold),
+                        UVec2::new(size.x, size.y - threshold),
+                    );
+                }
+                if rng.random_bool(0.5) {
+                    new_regions.push(first_region);
+                    new_regions.push(second_region);
+                } else {
+                    new_regions.push(second_region);
+                    new_regions.push(first_region);
+                }
+            }
+            regions = new_regions;
+        }
+        for (start, size) in regions.last() {
+            for x in start.x..(start.x + size.x) {
+                for y in start.y..(start.y + size.y) {
+                    self.set_tile(TilePos(UVec2::new(x, y)), TileKind::Floor);
+                }
+            }
+        }
+    }
+    pub fn generate_map(&mut self, size: UVec2) {
+        self.reset();
+        self.render_grid_size = size;
+        // map.generate_bsp_floor(5, UVec2::new(10, 10));
+
+        for x in 0..self.render_grid_size.x {
+            for y in 0..self.render_grid_size.y {
+                // let kind = if x == 0
+                //     || x == map.render_grid_size.x - 1
+                //     || y == 0
+                //     || y == map.render_grid_size.y - 1
+                // {
+                //     TileKind::Wall
+                // } else {
+                //     TileKind::Floor
+                // };
+
+                self.set_tile(TilePos(UVec2::new(x, y)), TileKind::Wall);
+            }
+        }
+        // map.set_tile(TilePos(UVec2::new(5, 5)), TileKind::Water);
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
@@ -36,27 +126,4 @@ pub enum TileKind {
     Wall,
     Water,
     Floor,
-}
-
-pub fn generate_map(map: &mut Map, size: UVec2) {
-    map.reset();
-    map.render_grid_size = size;
-
-    for x in 0..map.render_grid_size.x {
-        for y in 0..map.render_grid_size.y {
-            let kind = if x == 0
-                || x == map.render_grid_size.x - 1
-                || y == 0
-                || y == map.render_grid_size.y - 1
-            {
-                TileKind::Wall
-            } else {
-                TileKind::Floor
-            };
-
-            map.set_tile(TilePos(UVec2::new(x, y)), kind);
-        }
-    }
-
-    map.set_tile(TilePos(UVec2::new(5, 5)), TileKind::Water);
 }
